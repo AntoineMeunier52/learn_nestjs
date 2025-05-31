6be608ef1d8ca052ddcadaf3e0780c5dc1913777
AntoineMeunier52 <meunier.antoine52@gmail.com>
2025-05-31 14:55:30 +0200

    input and handle maliciours input

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //filter for input
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //filter/remove extra extra element in body
+      whitelist: true,
+      //primitif type conversion /!\huge impact on performance
+      //transform: true,
+      //reject an error if more element than needed in body
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
