c7ff2baf4a15d0ea72203d1f7d1e41284e06e2c2
AntoineMeunier52 <meunier.antoine52@gmail.com>
2025-05-31 09:42:17 +0200

    query

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,19 +1,23 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
+  Query,
   Res,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
-  @Get('flavors')
-  findAll(@Res() response) {
-    return 'This action returns all coffees';
+  @Get()
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
   }
 
   @Get(':id')
@@ -25,5 +29,15 @@ export class CoffeesController {
   create(@Body() body) {
     return body;
     //return `This action create a coffee`;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This action updates #${id} coffee`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action remove #${id} coffee`;
   }
 }
